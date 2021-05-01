5c32e43af3d98f982e945a009a254df394b11fe2
unknown <tranthanhdanh0311@gmail.com>
2021-04-30 19:48:57 -0700

    oke

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -14,7 +14,7 @@ const RoomsList = ({ rooms }) => {
     <section className="roomslist">
       <div className="roomslist-center">
         {rooms.map(item => {
-          return <Room key={item.id} room={item} />;
+          return <Room key={item.id} room={item} />
         })}
       </div>
     </section>
